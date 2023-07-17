b501b882ccbfd499ea2645172144f2c539cfb838
Tusti-Gupta <86391913+Tusti-Gupta@users.noreply.github.com>
2023-07-16 20:53:33 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -7,3 +7,33 @@ function startClassification() {
 function modelReady() {
     classifier.classify(gotResults);
 }
+
+function gotResults (error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_label").innerHTML = "I can hear - " + results[0].label;
+        document.getElementById("result_accuracy").innerHTML = "Accuracy - " + (results[0].confidence*100).toFixed(2) + "%";
+
+        if (results[0].label == "Background Noise") {
+            document.getElementById("result_image").src = "listen.gif";   
+        } else if (results[0].label == "Cat") { 
+            document.getElementById("result_image").src = "cat.jpg";
+        } else if (results[0].label == "Chicken") { 
+            document.getElementById("result_image").src = "chicken.png";
+        } else if (results[0].label == "Cow") { 
+            document.getElementById("result_image").src = "cow.jpg";
+        } else if (results[0].label == "Crow") { 
+            document.getElementById("result_image").src = "crow.jpg";
+        } else if (results[0].label == "Dog") { 
+            document.getElementById("result_image").src = "dog.png";
+        } else if (results[0].label == "Elephant") { 
+            document.getElementById("result_image").src = "elephant.jpg";
+        } else if (results[0].label == "Lion") { 
+            document.getElementById("result_image").src = "lion.jpg";
+        } else if (results[0].label == "Pig") { 
+            document.getElementById("result_image").src = "pig.jpg";
+        }
+    }
+}
